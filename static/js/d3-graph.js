863d092aaacc7fb9919c2e536668989357182203
Ewa Janikowska <ewa.janikowska7@gmail.com>
2016-12-26 18:26:58 +0100

    Style and layout

---
+++ static/js/d3-graph.js
@@ -0,0 +1,86 @@
+String.prototype.trunc = String.prototype.trunc ||
+  function(n){
+    return (this.length > n) ? this.substr(0,n-1)+'...' : this;
+  };
+
+var color = d3.scale.category10();
+var width = 700, height = 700, graph, node, link;
+
+var force = d3.layout.force()
+  .size([width, height])
+  .on("tick", tick)
+  .charge(-700)
+  .gravity(0.2)
+  .friction(0.9)
+  .linkDistance(20);
+
+function updateGraph(url) {
+  d3.json(url, function(error, g) {
+    var nodeMap = {};
+    g.nodes.forEach(function(x) { nodeMap[x.id] = x; });
+    g.links = g.links.map(function(x) {
+      return {
+        source: g.nodes.indexOf(nodeMap[x.source]),
+        target: g.nodes.indexOf(nodeMap[x.target])
+      };
+    });
+    graph = g;
+    update();
+  });
+}
+
+function update() {
+  link = svg.selectAll("line.link")
+    .remove();
+  node = svg.selectAll(".node")
+    .remove();
+  // Start the force layout.
+  force
+    .nodes(graph.nodes)
+    .links(graph.links)
+    .start();
+  // Update the links…
+  link = svg.selectAll("line.link")
+    .data(graph.links);//, function(d) { return d.target.id; });
+  // Enter any new links.
+  link.enter().insert("svg:line", ".node")
+    .attr("class", "link")
+    .attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; });
+  // Exit any old links.
+  link.exit().remove();
+  // Update the nodes…
+  node = svg.selectAll(".node")
+    .data(graph.nodes);
+
+  var nodeE = node.enter();
+
+  var nodeG = nodeE.append("g")
+    .attr("class", "node")
+    .call(force.drag);
+
+  nodeG.append("circle")  
+    .attr("r", 10)
+  //    .on("click", click)
+    .style("fill", function(d) { return color(d['class-name']);});
+
+  nodeG.append("text")
+    .attr("dy", 10 + 15)
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d.name.trunc(10) });
+
+  node.exit().remove();
+
+};
+
+function tick() {
+  link.attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; });
+
+  node.attr("transform", function(d) {
+    return "translate(" + d.x + "," + d.y + ")"; });
+};
